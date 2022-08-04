fcffb02564a073c49fb17926193cc7012a92df55
Uriel Aguilar <uaguilardeveloper@gmail.com>
2022-08-04 15:05:37 -0500

    Particles and images

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      initialRoute: '/videos',
+      initialRoute: '/',
       routes: {
         '/' : (context) => const LoginScreen(),
         '/videos':(context) => const VideosScreen(),
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,16 +1,52 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:animated_background/animated_background.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> 
+  with SingleTickerProviderStateMixin {
+
+    ParticleOptions particles = const ParticleOptions(
+    baseColor: Colors.white,
+    spawnOpacity: 0.0,
+    opacityChangeRate: 0.25,
+    minOpacity: 0.1,
+    maxOpacity: 0.4,
+    particleCount: 20,
+    spawnMaxRadius: 9.0,
+    spawnMaxSpeed: 50.0,
+    spawnMinSpeed: 15,
+    spawnMinRadius: 4.0,
+    );
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('LoginScreen'),
-      ),
-      body: Center(
-        child: Text('Login'),
+      body: SafeArea(
+        child: Stack(children: [
+          Image.network(
+            'https://meedios.com/wp-content/uploads/2020/10/yt.jpg',
+            height: double.infinity,
+            fit: BoxFit.fitHeight,
+          ),
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: Container(color: Color.fromARGB(123, 180, 176, 176)),
+            ),
+          ),
+          AnimatedBackground(child: Container(), vsync: this, behaviour: RandomParticleBehaviour(options: particles)),
+
+        ]),
       ),
     );
   }
